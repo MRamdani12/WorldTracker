ce21cb65cdb0315f1c6750744c243e4265636f18
MRamdani <mramdani1230@gmail.com>
2025-11-25 01:23:42 +0700

    Fixed fallback bug

--- src/components/ui/LoadingFullPage.tsx
+++ src/components/ui/LoadingFullPage.tsx
@@ -11,7 +11,6 @@ export default function LoadingFullPage({
     return (
         <div className={`${styles.loading} ${styles.wide}`}>
             <LoadAnimation />
-            Loading page...
             {children}
         </div>
     );
